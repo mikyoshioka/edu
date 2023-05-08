298cdb4fe9907df1a1d7e5b11ccbf11f1370f3b1
unknown <yoshioka@mik-s.jp>
2023-05-08 15:25:04 +0900

    up

---
+++ ddldml/ddl.sql
@@ -0,0 +1,12 @@
+CREATE TABLE sample
+(
+    id INT NOT NULL,
+    name VARCHAR(45),
+    PRIMARY KEY ( id )
+);
+
+COMMENT ON TABLE sample IS 'ƒTƒ“ƒvƒ‹';
+COMMENT ON COLUMN sample.id IS 'ID';
+COMMENT ON COLUMN sample.name IS '–¼‘O';
+
+
